932b3244bebaf6d1e34860aa0d2aade6955a62de
ginogiorgi <ginorubengiorgi@gmail.com>
2026-03-03 17:16:24 +0000

    Deployment fix

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,6 +1,6 @@
 import Card from "@/components/News/Card";
 import Pagination from "@/components/News/Pagination";
-import Title from "@/components/News/Title";
+import Title from "@/components/common/Title";
 
 const NewsPage = () => {
     return (
