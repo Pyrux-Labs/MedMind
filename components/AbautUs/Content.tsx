f40cf238e651a8bbbc2daf8aca8ec0691febdf47
ginogiorgi <ginorubengiorgi@gmail.com>
2026-03-11 20:25:47 +0000

    previes AbautUs content

--- components/AbautUs/Content.tsx
+++ components/AbautUs/Content.tsx
@@ -7,9 +7,23 @@ const Content = () => {
     const t = useTranslations("about.founders");
 
     return (
-        <div className="my-30 flex flex-col items-center">
-            <Founder name={t("guillermina.name")} bio={t("guillermina.bio")} />
-            <Founder name={t("lucia.name")} bio={t("lucia.bio")} />
+        <div className="my-30 flex gap-37">
+            <Founder
+                name={t("guillermina.name")}
+                bio={t("guillermina.bio")}
+                imageSrc={"/test.jpeg"}
+                email={"pyrux@pyrux.com.ar"}
+                linkedIn={
+                    "https://www.linkedin.com/in/guillermina-bassi-65908b195/"
+                }
+            />
+            <Founder
+                name={t("lucia.name")}
+                bio={t("lucia.bio")}
+                imageSrc={"/test.jpeg"}
+                email={"pyrux@pyrux.com.ar"}
+                linkedIn={"https://www.linkedin.com/in/luc%C3%ADabiselli/"}
+            />
         </div>
     );
 };
--- components/AbautUs/Founder.tsx
+++ components/AbautUs/Founder.tsx
@@ -1,15 +1,44 @@
+import Image from "next/image";
+import Title from "../common/Title";
+
 interface FounderProps {
-	name: string;
-	bio: string;
+    name: string;
+    bio: string;
+    imageSrc: string;
+    email: string;
+    linkedIn: string;
 }
 
-const Founder = ({ name, bio }: FounderProps) => {
-	return (
-		<div>
-			<h3>{name}</h3>
-			<p>{bio}</p>
-		</div>
-	);
+const Founder = ({ name, bio, imageSrc, email, linkedIn }: FounderProps) => {
+    const namespace = name;
+
+    return (
+        <div className="w-130 flex flex-col gap-10">
+            <Image
+                src={imageSrc}
+                alt={namespace}
+                width={529}
+                height={529}
+                className="object-cover rounded-2xl shadow-custom"
+            />
+            <Title text={name} align="left" />
+            <p className="text">{bio}</p>
+            <div className="flex gap-4">
+                <Image
+                    src="/social/linkedin.svg"
+                    alt="LinkedIn"
+                    height={28}
+                    width={28}
+                />
+                <Image
+                    src="/social/email.svg"
+                    alt="Email"
+                    height={28}
+                    width={28}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Founder;
