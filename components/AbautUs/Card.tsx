977dd93370d33492517db72b7ceff4cac4715318
ginogiorgi <ginorubengiorgi@gmail.com>
2026-03-11 18:41:12 +0000

    Cards container

--- components/AbautUs/Card.tsx
+++ components/AbautUs/Card.tsx
@@ -1,19 +1,21 @@
+import Image from "next/image";
+
 interface CardProps {
     title: string;
     description: string;
+    imageSrc: string;
 }
 
-const Card = ({ title, description }: CardProps) => {
+const Card = ({ title, description, imageSrc }: CardProps) => {
     return (
-        <div className="group w-80 h-44 perspective-[1000px]">
-            <div className="relative w-full h-full transition-transform duration-700 transform-3d group-hover:[transform:rotateY(180deg)]">
-                {/* Front */}
-                <div className="absolute inset-0 flex items-center justify-center p-4 [backface-visibility:hidden] border-2 border-main-color shadow-custom rounded-3xl">
+        <div className="group w-91 h-67 perspective-[1000px]">
+            <div className="w-full h-full duration-700 transform-3d group-hover:transform-[rotateY(180deg)]">
+                <div className="absolute inset-0 flex items-center justify-center backface-hidden border-2 border-main-color shadow-custom rounded-3xl flex-col gap-3.5">
+                    <Image src={imageSrc} alt="check" height={60} width={60} />
                     <h3 className="contact_us">{title}</h3>
                 </div>
-                {/* Back */}
-                <div className="absolute inset-0 flex items-center justify-center p-4 [transform:rotateY(180deg)] [backface-visibility:hidden] border-2 border-main-color shadow-custom rounded-3xl">
-                    <p className="text">{description}</p>
+                <div className="absolute inset-0 flex items-center justify-center p-5 transform-[rotateY(180deg)] backface-hidden border-2 border-main-color shadow-custom rounded-3xl">
+                    <p className="text border-l-2 pl-5">{description}</p>
                 </div>
             </div>
         </div>
--- components/AbautUs/CardsContainer.tsx
+++ components/AbautUs/CardsContainer.tsx
@@ -10,18 +10,21 @@ const CardsContainer = () => {
         <div className="my-30 justify-items-center flex flex-col gap-11">
             <h2 className="main-title text-center">{t("personality.title")}</h2>
             <p className="text text-center">{t("personality.description")}</p>
-            <div>
+            <div className="flex justify-center gap-12">
                 <Card
                     title={t("purpose.title")}
                     description={t("purpose.description")}
+                    imageSrc={"/abaut/cards/purpose.svg"}
                 />
                 <Card
                     title={t("mission.title")}
                     description={t("mission.description")}
+                    imageSrc={"/abaut/cards/mission.svg"}
                 />
                 <Card
                     title={t("vision.title")}
                     description={t("vision.description")}
+                    imageSrc={"/abaut/cards/vision.svg"}
                 />
             </div>
         </div>
