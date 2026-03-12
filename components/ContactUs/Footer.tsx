35bd5c4a4e7cafe2fe5844cb0f24217868188052
ginogiorgi <ginorubengiorgi@gmail.com>
2026-03-12 04:01:34 +0000

    Contact footer

--- components/ContactUs/Footer.tsx
+++ components/ContactUs/Footer.tsx
@@ -1,18 +1,34 @@
 "use client";
-
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 const Footer = () => {
-	const t = useTranslations("contact.info");
+    const t = useTranslations("contact.info");
 
-	return (
-		<div>
-			<h3>{t("title")}</h3>
-			<p>{t("email")}</p>
-			<p>{t("location")}</p>
-			<p>{t("country")}</p>
-		</div>
-	);
+    return (
+        <div className="my-30 flex justify-center gap-84">
+            <div className="gap-5 flex flex-col text-center items-center">
+                <Image
+                    src="/contact/footer/contact.svg"
+                    alt="contact.svg"
+                    height={114}
+                    width={114}
+                />
+                <h2 className="subtitle">{t("contact")}</h2>
+                <p className="label text-footer-labels!">info@medmindls.com</p>
+            </div>
+            <div className="gap-5 flex flex-col text-center items-center">
+                <Image
+                    src="/contact/footer/location.svg"
+                    alt="contact.svg"
+                    height={114}
+                    width={114}
+                />
+                <h2 className="subtitle">{t("location")}</h2>
+                <p className="label text-footer-labels!">{t("country")}</p>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
